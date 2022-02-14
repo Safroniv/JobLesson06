be5e7d72478dead151981e3022a1b410ea3f9ce4
Safroniv <safroniv@mail.ru>
2022-02-14 19:57:34 +0300

    Добавьте файлы проекта.

---
+++ JobLesson06Part01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace JobLesson06Part01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string log = string.Empty; // пустая строка - ""
+            log += $"Начало работы приложения";
+            try
+            {
+                int x = 10;
+                int y = x / 0;
+                log += $"{Environment.NewLine}Результат деления: {y}";
+            }
+            catch
+            {
+                log += $"{Environment.NewLine}Во время выполнения деления произошла ошибка";
+            }
+            finally
+            {
+                File.WriteAllText("log.txt", log);
+            }
+        }
+    }
+}
